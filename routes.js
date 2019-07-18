b0c5ac120c81d28ab774131742bd2ac97150bf4e
Dylan-Speight <d.speight92@gmail.com>
2019-07-18 14:20:59 +1000

    homeroute

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const UserController = require("./controllers/user_controller")
 const InvestmentController = require("./controllers/investment_controller")
 
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+
 router.get("/users", UserController.index)
 
 router.post("/users", UserController.create)
