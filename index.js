f4654c2f0dc4a611060bbe1bdf870b9a2e8ec169
Aaron <gitkraken@aaronlcjones.com>
2019-07-18 13:20:46 +1000

    Auto stash before merge of "TestDB" and "origin/TestDB"

--- index.js
+++ index.js
@@ -1,20 +1,106 @@
 const mongoose = require("mongoose");
 const express = require("express")
 const bodyParser = require("body-parser")
-const jwt = require('express-jwt');
+// const jwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const withAuth = require('./middleware/auth');
 
-const Schema = mongoose.Schema;
+const cors = require('cors');
+const User = require('./models/User.js');
+
+const corsOptions = {
+    origin: 'http://localhost:3000'
+}
+// const Schema = mongoose.Schema;
+const secret = 'mysecretsshhh';
 
 const app = express()
 const port = 4000
+app.use(cors());
+app.use(cookieParser());
+const mongo_uri = "mongodb://localhost/usersdb"
+mongoose.connect(mongo_uri, function(err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log(`Successfully connected to ${mongo_uri}`);
+    }
+  });
+// mongoose.connect("mongodb://localhost/usersdb");
+// mongoose.connection.on("error", err => console.log(err))
 
-mongoose.connect("mongodb://localhost/usersdb");
-mongoose.connection.on("error", err => console.log(err))
 
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors(corsOptions));
 
 app.use(require("./routes"))
 
+app.get('/api/home',(req, res, next) => {
+    res.send('Welcome!');
+});
+app.get('/api/secret', withAuth, function(req, res) {
+  res.send('The password is potato');
+});
+
+app.get('/checkToken', withAuth, function(req, res) {
+    res.sendStatus(200);
+});
+
+
+  // POST route to register a user
+app.post('/api/register', function(req, res) {
+    const { email, password } = req.body;
+    const user = new User({ email, password });
+    user.save(function(err) {
+        console.log(err)
+        if (err) {
+            res.status(500)
+            .send("Error registering new user please try again.");
+        } else {
+            res.status(200).send("Welcome to the club!");
+        }
+    });
+});
+app.post('/api/authenticate', function(req, res) {
+    const { email, password } = req.body;
+    User.findOne({ email }, function(err, user) {
+        if (err) {
+            console.error(err);
+            res.status(500)
+            .json({
+            error: 'Internal error please try again'
+            });
+        } else if (!user) {
+            res.status(401)
+            .json({
+                error: 'Incorrect email or password'
+            });
+        } else {
+            user.isCorrectPassword(password, function(err, same) {
+            if (err) {
+                res.status(500)
+                .json({
+                    error: 'Internal error please try again'
+                });
+            } else if (!same) {
+                res.status(401)
+                .json({
+                    error: 'Incorrect email or password'
+                });
+            } else {
+                // Issue token
+                const payload = { email };
+                const token = jwt.sign(payload, secret, {
+                expiresIn: '1h'
+                });
+                res.cookie('token', token, { httpOnly: true })
+                .sendStatus(200);
+            }
+            });
+        }
+    });
+});
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const UserModel = mongoose.model('Users',UserSchema);
+
+UserSchema.methods.isCorrectPassword = function(password, callback){
+    bcrypt.compare(password, this.password, function(err, same) {
+        if (err) {
+            callback(err);
+        } else {
+            callback(err, same);
+        }
+    });
+}
+
+UserSchema.pre('save', function(next) {
+    // console.log(UserModel.find({"email": user.email}))
+    // console.log(user)
+    // UserModel.find({"email": user.email}), function (err, docs) {
+    //     if (docs.length){
+    //         cb('Name exists already',null);
+    //     }else{
+    //         user.save(function(err){
+    //             cb(err,user);
+    //         });
+    //     }
+    // }
+    // Check if document is new or a new password has been set
+    if (this.isNew || this.isModified('password')) {
+      // Saving reference to this because of changing scopes
+      const document = this;
+      bcrypt.hash(document.password, saltRounds,
+        function(err, hashedPassword) {
+        if (err) {
+            res.status(500).send('User already exists');
+            // console.log(err)
+          next(err);
+        }
+        else {
+          document.password = hashedPassword;
+          next();
+        }
+      });
+    } else {
+      next();
+    }
+  });
+
+module.exports = mongoose.model('User', UserSchema);
