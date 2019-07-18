faabed936e7b6c41fc07f7bd852449e54e721330
Dylan-Speight <d.speight92@gmail.com>
2019-07-18 14:24:47 +1000

    homeroute fix

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const UserController = require("./controllers/user_controller")
 const InvestmentController = require("./controllers/investment_controller")
-
+const app = express()
 app.get('/', function (req, res) {
     res.send('hello world')
   })
