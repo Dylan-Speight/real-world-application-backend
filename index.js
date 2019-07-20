ecf50a4b6cb196561c6880695cd7722ac3e1b605
Aaron <gitkraken@aaronlcjones.com>
2019-07-21 06:26:09 +1000

    Started creating profile database queries

--- index.js
+++ index.js
@@ -115,4 +115,100 @@ app.post('/api/authenticate', function(req, res) {
         }
     });
 });
+
+// app.post('/api/saveinvestment', function(req, res) {
+//     const { investment } = req.body;
+//     const formattedInvestment = {
+//         propertyData: [
+//             {
+//               price: {
+//                 income: investment.income,
+//                 expenses: investment.expenses,
+//                 value: investment.value,
+//                 roi: investment.roi
+//               },
+//               address: {
+//                 state: investment.address.state,
+//                 postcode: investment.address.postcode,
+//                 suburb: investment.address.suburb,
+//                 street: investment.address.street,
+//                 number: investment.address.number
+//               },
+//               media: [
+//                   investment.media.map()
+//               ],
+//               description: String,
+//               id: String
+//             }
+//           ]
+//     }
+//     const { email } = req.headers.authorization;
+//     // const newinvestment = new Investment({ id: user, investment: investment });
+//     User.update({ email }, { $set: { investments : investment }
+//         // , function(err, user) {
+//         // newinvestment.save(function(err) {
+//         //     if (err) {
+//         //         res.status(500).json({error:"Error saving investment please try again."});
+//         //     } else {
+//         //         res.status(200).send("Investment added to your profile.");
+//         //     }
+//         // });
+//     });
+// });
+app.post('/api/findinvestment', function(req, res) {
+    console.log(req.headers.authorization)
+    console.log("looking for investments")
+    const { email } = req.headers.authorization;
+    User.findOne( email , function(err, user) {
+        if (err) {
+            console.log("User doesn't exist")
+        }
+        else {
+            console.log("found email")
+            console.log(user._id)
+            // Investment.findOne( email , function(err, user) {
+
+        }
+    })
+})
+//             console.error(err);
+//             res.status(500)
+//             .json({
+//             error: 'Internal error please try again'
+//             });
+//         } else if (!user) {
+//             console.log("Wrong deets")
+
+//             res.status(401)
+//             .json({
+//                 error: 'Incorrect email or password'
+//             });
+//         } else {
+//             user.isCorrectPassword(password, function(err, same) {
+//                 console.log("CorrectPass")
+//             if (err) {
+//                 res.status(500)
+//                 .json({
+//                     error: 'Internal error please try again'
+//                 });
+//             } else if (!same) {
+//                 console.log("typo somewhere")
+
+//                 res.status(401)
+//                 .json({
+//                     error: 'Incorrect email or password'
+//                 });
+//             } else {
+//                 console.log("token issued")
+//                 const payload = { email };
+//                 const token = jwt.sign(payload, secret, {
+//                 expiresIn: '1h'
+//                 })
+//                 console.log(token)
+//                 res.status(200).json({'token': token})
+//             }
+//             });
+//         }
+//     });
+// });
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
